194d14710ddd3feeb293ded75eeb3481b5c55cc3
sgmikami03 <74947937+sgmikami03@users.noreply.github.com>
2020-11-25 19:00:59 +0900

    課題2-11:フォームに入力した内容を取得する

---
+++ 2-11/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="ja">
+  <head>
+    <meta charset="utf-8">
+    <title>入力</title>
+  </head>
+  <body>
+    <form action="result.php" method="post">
+      名前:<input type="text" name="name"><br>
+      メールアドレス:<input type="text" name="mail"><br>
+      パスワード:<input type="text" name="pass"><br>
+      <input type='submit' value="送信">
+    </form>
+  </body>
+</html>
---
+++ 2-11/result.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="ja">
+  <head>
+    <meta charset="utf-8">
+    <title>確認</title>
+  </head>
+  <body>
+    私の名前は、<?php echo $_POST['name'] ;?><br>
+    メールアドレスは、<?php echo $_POST['mail'] ;?><br>
+    パスワードは、<?php echo $_POST['pass'] ;?>
+  </body>
+</html>
